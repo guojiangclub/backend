cc9b3449881d591123dd9ad399c4a5b7c7262114
peter.chen <i@chenhow.com>
2019-10-09 17:42:26 +0800

    果酱页面添加小程序太阳码

--- resources/views/guojiang.blade.php
+++ resources/views/guojiang.blade.php
@@ -14,3 +14,56 @@
     </div>
 
 </div>
+
+<div class="row" style="margin-top: 10px;">
+    <div class="col-md-2">
+        <div class="thumbnail">
+            <img src="{{admin_asset('vendor/img/ec.qrcode.png')}}" alt="..." class="img-responsive">
+            <div class="caption">
+                <h3 class="text-center">果酱小店</h3>
+                {{--<p>...</p>--}}
+                {{--<p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>--}}
+            </div>
+        </div>
+    </div>
+    <div class="col-md-2">
+        <div class="thumbnail">
+            <img src="{{admin_asset('vendor/img/activity.qrcode.jpg')}}" alt="..." class="img-responsive">
+            <div class="caption">
+                <h3 class="text-center">果酱互动</h3>
+                {{--<p>...</p>--}}
+                {{--<p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>--}}
+            </div>
+        </div>
+    </div>
+    <div class="col-md-2">
+        <div class="thumbnail">
+            <img src="{{admin_asset('vendor/img/coterie.qrcode.jpg')}}" alt="..." class="img-responsive">
+            <div class="caption">
+                <h3 class="text-center">果酱圈子</h3>
+                {{--<p>...</p>--}}
+                {{--<p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>--}}
+            </div>
+        </div>
+    </div>
+    <div class="col-md-2">
+        <div class="thumbnail">
+            <img src="{{admin_asset('vendor/img/box.qrcode.jpg')}}" alt="..." class="img-responsive">
+            <div class="caption">
+                <h3 class="text-center">果酱盒子</h3>
+                {{--<p>...</p>--}}
+                {{--<p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>--}}
+            </div>
+        </div>
+    </div>
+    <div class="col-md-2">
+        <div class="thumbnail">
+            <img src="{{admin_asset('vendor/img/album.qrcode.jpg')}}" alt="..." class="img-responsive">
+            <div class="caption">
+                <h3 class="text-center">果酱相册</h3>
+                {{--<p>...</p>--}}
+                {{--<p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>--}}
+            </div>
+        </div>
+    </div>
+</div>
