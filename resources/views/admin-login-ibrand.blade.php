dfdad857351e6896bbc3d760ab150d1d79a4b583
lifeagin <yaochao133@163.com>
2019-10-09 09:55:07 +0800

    添加后台登陆页静态文件

--- resources/views/admin-login-ibrand.blade.php
+++ resources/views/admin-login-ibrand.blade.php
@@ -18,7 +18,7 @@
     <link rel="stylesheet" href="{{ admin_asset("/vendor/laravel-admin/toastr/build/toastr.min.css") }}">
 
     <link rel="stylesheet" href="{{ admin_asset("/vendor/css/login.css") }}">
-
+    <link rel="stylesheet" type="text/css" href="{{admin_asset("/vendor/css/swiper.min.css")}}" />
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
@@ -27,78 +27,90 @@
     <![endif]-->
 </head>
 
-<div id="content">
 
-    <div class="login-wrap">
-        <div class="w clearfix">
-            <div class="text-box">
-                <div class="hello-box">
-                    <div class="hello">
-                        HELLO
+
+
+<div id="content">
+    <div class="login-left">
+        <div class="swiper-container-solution">
+            <div class="selected-solution">
+                <div>
+                    <div class="swiper-container">
+
+                        <div class="solution-cardlist swiper-wrapper">
+                            <div class="col-sm-ss swiper-slide">
+                                <div class="solution-card">
+                                    <img src="http://dev.edu.com/vendor/img/1.png" width="100%">
+                                    <h3>为消费升级赋能<br>品牌新零售技术解决方案</h3>
+                                    </span>
+                                </div>
+                            </div>
+                            <div class="col-sm-ss swiper-slide">
+                                <div class="solution-card">
+                                    <img src="http://dev.edu.com/vendor/img/2.png" width="100%">
+                                    <h3>提供漂亮的页面动态效果</h3>
+
+                                </div>
+                            </div>
+
+                        </div>
+                        <div class="swiper-pagination solution-pagination"></div>
                     </div>
-                    <div>
-                        欢迎登陆后台系统
-                    </div>
-                </div>
-                <div class="info">
-                    技术支持：上海艾游文化传播有限公司
+                    <div class="swiper-button-prev"></div>
+                    <div class="swiper-button-next"></div>
                 </div>
             </div>
+        </div>
+    </div>
 
-            <form id="login_form"  action="{{ admin_base_path('auth/login') }}" method="post" onsubmit="return check(this)">
+    <div class="login-right">
+        <form id="login_form" action="{{ admin_base_path('auth/login') }}" method="post" onsubmit="return check(this)">
 
-                <div class="login-form">
+            <div class="login-form">
 
-                    <div class="login-box">
-                        <div class="logo">
-                            <img src="{{ admin_asset("/vendor/img/logo.png") }}" alt="">
-                        </div>
-                        <div class="inptu-box">
+                <div class="login-box">
+                    <div class="logo">
+                        <img src="{{ admin_asset("/vendor/img/logo.png") }}" alt="">
+                    </div>
+                    <div class="inptu-box">
 
-                            <input type="text" placeholder="邮箱/用户名"  name="username" value="{{ old('username') }}">
-                        </div>
-
-
-
-                        <div class="inptu-box">
-
-                            <input type="password" placeholder="密码" name="password">
-
-                        </div>
+                        <input type="text" placeholder="邮箱/用户名" name="username" value="{{ old('username') }}">
+                    </div>
 
 
-                        @if(config('ibrand.backend.sms_login'))
+                    <div class="inptu-box">
+
+                        <input type="password" placeholder="密码" name="password">
+
+                    </div>
+
+
+                    @if(config('ibrand.backend.sms_login'))
 
                         <div class="inptu-box code-box">
 
                             <input type="text" placeholder="验证码" name="code" value="{{ old('code') }}">
 
-                            <button type="button" id="send-verifi" style="border: none" class="code" data-target="login" data-status=0>发送验证码</button>
+                            <button type="button" id="send-verifi" style="border: none" class="code" data-target="login"
+                                    data-status=0>发送验证码
+                            </button>
 
                         </div>
 
-                        @endif
+                    @endif
 
 
-                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-                        {{--<div class="login">--}}
-                            <button type="submit" class="login">登录</button>
-                        {{--</div>--}}
-
+                    <div class="login">
+                        <button type="submit" class="login">登录</button>
                     </div>
-                </div>
-            </form>
 
-
-        </div>
-        <div class="login-banner">
-            <div class="w">
-                <div id="banner-bg" class="i-inner">
                 </div>
             </div>
-        </div>
+        </form>
     </div>
+
 </div>
 </body>
 
@@ -112,132 +124,159 @@
 <script src="{{ admin_asset("/vendor/laravel-admin/AdminLTE/plugins/iCheck/icheck.min.js")}}"></script>
 
 <script src="{{ admin_asset("/vendor/laravel-admin/toastr/build/toastr.min.js")}}"></script>
-
+<script src="{{ admin_asset("/vendor/libs/swiper.min.js")}}" type="text/javascript" charset="utf-8"></script>
 <script>
-        function check(form) {
-            if(form.username.value==''){
-                toastr.warning("请输入邮箱或用户名");
-                return false;
-            }
-            if(form.password.value==''){
-                toastr.warning("请输入密码");
-                return false;
-            }
-            return true;
+    function check(form) {
+        if (form.username.value == '') {
+            toastr.warning("请输入邮箱或用户名");
+            return false;
         }
+        if (form.password.value == '') {
+            toastr.warning("请输入密码");
+            return false;
+        }
+        return true;
+    }
 
-        @if($errors->has('username'))
-        @foreach($errors->get('username') as $message)
-        toastr.error("{{$message}}");
-        @endforeach
-        @endif
+    @if($errors->has('username'))
+    @foreach($errors->get('username') as $message)
+    toastr.error("{{$message}}");
+    @endforeach
+    @endif
 
-        @if($errors->has('code'))
-        @foreach($errors->get('code') as $message)
-        toastr.error("{{$message}}");
-        @endforeach
-        @endif
+    @if($errors->has('code'))
+    @foreach($errors->get('code') as $message)
+    toastr.error("{{$message}}");
+    @endforeach
+    @endif
 
-        @if($errors->has('password'))
-        @foreach($errors->get('password') as $message)
-        toastr.error("{{$message}}");
-        @endforeach
-        @endif
+    @if($errors->has('password'))
+    @foreach($errors->get('password') as $message)
+    toastr.error("{{$message}}");
+    @endforeach
+            @endif
 
         window.AppUrl = "{{env('APP_URL')}}";
-        window._token = "{{ csrf_token() }}";
-        var postUrl = '{{env('APP_URL')}}/getMobile';
+    window._token = "{{ csrf_token() }}";
+    var postUrl = '{{env('APP_URL')}}/getMobile';
 
-        @if(config('ibrand.backend.sms_login'))
-        $(document).ready(function () {
-            // 发送验证码
-            $('#send-verifi').on('click', function () {
-                var el = $(this);
-                var target = el.data('target');
-                var mobileReg = /^(?=\d{11}$)^1(?:3\d|4[57]|5[^4\D]|66|7[^249\D]|8\d|9[89])\d{8}$/;
+    @if(config('ibrand.backend.sms_login'))
+    $(document).ready(function () {
+        // 发送验证码
+        $('#send-verifi').on('click', function () {
+            var el = $(this);
+            var target = el.data('target');
+            var mobileReg = /^(?=\d{11}$)^1(?:3\d|4[57]|5[^4\D]|66|7[^249\D]|8\d|9[89])\d{8}$/;
 
-                if(target == 'login'){ //  如果是登录
-                    $.ajax({
-                        type: 'post',
-                        data: {
-                            username: $('input[name="username"]').val(),
-                            _token:_token
-                        },
-                        url: postUrl,
-                        success: function (res) {
-                            if (res.status) {
-                                $('input[name="mobile"]').val(res.data.mobile);
-                                sendCode(el,res.data.mobile);
-                            } else {
-                                toastr.error(res.msg);
-                            }
-                        },
-                        error: function () {
-                            toastr.error('账号验证失败');
-                        }
-                    })
-
-                } else {
-                    var mobile = $('input[data-type=' + target + ']').val();
-                    if (mobile == ''){
-                        toastr.error('请输入手机号码');
-                        return
-                    }
-                    if (!mobileReg.test(mobile)) {
-                        toastr.error('请输入正确的手机号码');
-                    } else {
-                        sendCode(el,mobile);
-                    }
-                }
-            });
-
-            //发送验证码方法
-            function sendCode(el,mobile) {
-                if (el.data('status') != 0) {
-                    return
-                }
-                el.text('正在发送...');
-                el.data('status', '1');
+            if (target == 'login') { //  如果是登录
                 $.ajax({
-                    type: 'POST',
+                    type: 'post',
                     data: {
-                        mobile: mobile,
-                        access_token: _token
+                        username: $('input[name="username"]').val(),
+                        _token: _token
                     },
-                    url: AppUrl+"/{{config('ibrand.sms.route.prefix')}}/verify-code?_token="+_token,
-                    success: function (data) {
-                        if (data.success) {
-                            $('input[name="access_token"]').val(_token);
-                            var total = 60;
-                            var message = '请等待{#counter#}秒';
-                            el.text(message.replace(/\{#counter#}/g, total));
-                            var timer = setInterval(function () {
-                                total--;
-                                el.text(message.replace(/\{#counter#}/g, total));
-
-                                if (total < 1) {
-                                    el.data('status', '0');
-                                    el.text('发送验证码');
-                                    clearInterval(timer);
-                                }
-                            },1000)
+                    url: postUrl,
+                    success: function (res) {
+                        if (res.status) {
+                            $('input[name="mobile"]').val(res.data.mobile);
+                            sendCode(el, res.data.mobile);
                         } else {
-                            el.data('status', '0');
-                            el.text('发送验证码');
-                            toastr.error('短信发送失败！');
+                            toastr.error(res.msg);
                         }
                     },
                     error: function () {
+                        toastr.error('账号验证失败');
+                    }
+                })
+
+            } else {
+                var mobile = $('input[data-type=' + target + ']').val();
+                if (mobile == '') {
+                    toastr.error('请输入手机号码');
+                    return
+                }
+                if (!mobileReg.test(mobile)) {
+                    toastr.error('请输入正确的手机号码');
+                } else {
+                    sendCode(el, mobile);
+                }
+            }
+        });
+
+        //发送验证码方法
+        function sendCode(el, mobile) {
+            if (el.data('status') != 0) {
+                return
+            }
+            el.text('正在发送...');
+            el.data('status', '1');
+            $.ajax({
+                type: 'POST',
+                data: {
+                    mobile: mobile,
+                    access_token: _token
+                },
+                url: AppUrl + "/{{config('ibrand.sms.route.prefix')}}/verify-code?_token=" + _token,
+                success: function (data) {
+                    if (data.success) {
+                        $('input[name="access_token"]').val(_token);
+                        var total = 60;
+                        var message = '请等待{#counter#}秒';
+                        el.text(message.replace(/\{#counter#}/g, total));
+                        var timer = setInterval(function () {
+                            total--;
+                            el.text(message.replace(/\{#counter#}/g, total));
+
+                            if (total < 1) {
+                                el.data('status', '0');
+                                el.text('发送验证码');
+                                clearInterval(timer);
+                            }
+                        }, 1000)
+                    } else {
                         el.data('status', '0');
                         el.text('发送验证码');
                         toastr.error('短信发送失败！');
                     }
-                })
-            };
+                },
+                error: function () {
+                    el.data('status', '0');
+                    el.text('发送验证码');
+                    toastr.error('短信发送失败！');
+                }
+            })
+        };
 
-        });
+    });
     @endif
 
+</script>
+<script type="text/javascript">
+    $(function () {
+        new Swiper(".swiper-container", {
+
+            //默认功能属性
+            speed: 250,
+            slidesPerView: 1,
+            spaceBetween: 0,
+            slidesPerGroup: 1,
+            loop: true,
+            autoplay: true,
+
+            //分页索引
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true
+            },
+
+            //分页按钮
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev"
+            }
+
+        });
+    })
 </script>
 
 
